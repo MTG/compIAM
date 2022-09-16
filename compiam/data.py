7b1c969bc1967bfb5c84dc7fcebc6412b522bee1
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-09-16 15:52:21 +0200

    minor fix

--- compiam/data.py
+++ compiam/data.py
@@ -4,7 +4,6 @@
 # Each model should be stored in models_dict using <name>:<d> where <d> is:
 #	{'filepath': '<path_to_model>', 'wrapper': model wrapper}
 # see compiam.model_store.wrappers for more information on wrapper formatting and API
-from compiam.melody import ftanetCarnatic
 from compiam.rhythm import fourWayTabla
 
 models_dict = {
@@ -17,10 +16,10 @@ models_dict = {
 		'filepath': 'models/rhythm/4wayTabla/4way/',
 		'wrapper': fourWayTabla,
 		'kwargs': {}
-	},
+},
 	'melody:ftanet-carnatic': {
 		'filepath': 'models/melody/ftanet/carnatic/',
-		'wrapper': ftanetCarnatic,
+		'wrapper': None,#ftanetCarnatic,
 		'kwargs': {}
 	}
 }
