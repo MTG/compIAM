d55b83b119d7dafb7d9774a6a14be6e56ef5f8ea
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-09-29 18:55:49 +0200

    testing config

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-    import uuid
-    install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-    reqs = [str(req.req) for req in install_reqs]
-except: # for pip <= 9.0.3
-    def parse_requirements(filename):
-        """ load requirements from a pip requirements file"""
-        lineiter = (line.strip() for line in open(filename))
-        return [line for line in lineiter if line and not line.startswith("#")]
-    reqs = parse_requirements('requirements.txt')
-
 from setuptools import find_packages, setup
 
 setup(
@@ -20,5 +8,40 @@ setup(
     zip_safe=False,
     include_package_data=True,
     long_description=open('README.md').read(),
-    install_requires=reqs
+    install_requires = [
+        "matplotlib==3.4.2",
+        "numpy==1.18.5",
+        "librosa==0.8.0",
+        "SoundFile==0.10.3.post1",
+        "joblib==0.17.0",
+        "pathlib==1.0.1",
+        "pytsmod==0.3.3",
+        #scipy==1.4.1
+        #torch==1.8.0
+        "tqdm==4.64.1",
+        "mirdata==0.3.6",
+        "essentia"
+    ],
+    extras_require={
+        "tests": [
+            "pytest>=4.4.0",
+            "pytest-cov>=2.6.1",
+            "pytest-pep8>=1.0.0",
+            "pytest-mock>=1.10.1",
+            "pytest-localserver>=0.5.0",
+            "testcontainers>=2.3",
+            "future==0.17.1",
+            "coveralls>=1.7.0",
+            "types-PyYAML",
+            "types-chardet",
+            "smart_open[all] >= 5.0.0",
+        ],
+        "docs": [
+            "numpydoc",
+            "recommonmark",
+            "sphinx>=3.4.0",
+            "sphinxcontrib-napoleon",
+            "sphinx_rtd_theme",
+        ],
+    }
 )
