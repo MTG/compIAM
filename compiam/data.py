c9af2ee0230187301191eaa6c787012d9f6d3a7a
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-09-30 20:08:10 +0200

    add tests for wrappers

--- compiam/data.py
+++ compiam/data.py
@@ -2,35 +2,35 @@
 # Models Dict
 #############
 # Each model should be stored in models_dict using <name>:<d> where <d> is:
-#	{'filepath': '<path_to_model>', 'wrapper': model wrapper}
+#	{"filepath": "<path_to_model>", "wrapper": model wrapper}
 from compiam.melody import ftanetCarnatic, Melodia, TonicIndianMultiPitch
 from compiam.rhythm import fourWayTabla
 from compiam.timbre import MridangamStrokeClassification
 
 models_dict = {
-    'rhythm:1way-tabla': {
-        'wrapper': fourWayTabla,
-        'kwargs': {'filepath': 'models/rhythm/4wayTabla/1way/'}
+    "rhythm:1way-tabla": {
+        "wrapper": fourWayTabla,
+        "kwargs": {"filepath": "models/rhythm/4wayTabla/1way/"}
     },
-    'rhythm:4way-tabla': {
-        'wrapper': fourWayTabla,
-        'kwargs': {'filepath': 'models/rhythm/4wayTabla/4way/'}
+    "rhythm:4way-tabla": {
+        "wrapper": fourWayTabla,
+        "kwargs": {"filepath": "models/rhythm/4wayTabla/4way/"}
     },
-    'melody:ftanet-carnatic': {
-        'wrapper': ftanetCarnatic,
-        'kwargs': {'filepath': 'models/melody/ftanet/carnatic/'}
+    "melody:ftanet-carnatic": {
+        "wrapper": ftanetCarnatic,
+        "kwargs": {"filepath": "models/melody/ftanet/carnatic/"}
     },
-    'melody:melodia': {
-        'wrapper': Melodia,
-        'kwargs': {}
+    "melody:melodia": {
+        "wrapper": Melodia,
+        "kwargs": {}
     },
-    'melody:tonic-multipitch': {
-        'wrapper': TonicIndianMultiPitch,
-        'kwargs': {}
+    "melody:tonic-multipitch": {
+        "wrapper": TonicIndianMultiPitch,
+        "kwargs": {}
     },
-    'timbre:mridangam-stroke': {
-        'wrapper': MridangamStrokeClassification,
-        'kwargs': {}
+    "timbre:mridangam-stroke": {
+        "wrapper": MridangamStrokeClassification,
+        "kwargs": {}
     },
 }
 
@@ -42,7 +42,7 @@ models_dict = {
 #   1. create a dataset loader in mirdata (https://github.com/mir-dataset-loaders/mirdata) 
 #   2. add the dataset identifier in this list
 
-datasets_list = ['saraga_carnatic', 'saraga_hindustani', 'mridangam_stroke']
+datasets_list = ["saraga_carnatic", "saraga_hindustani", "mridangam_stroke"]
 
 
 ##############
@@ -50,36 +50,36 @@ datasets_list = ['saraga_carnatic', 'saraga_hindustani', 'mridangam_stroke']
 ##############
 
 corpora_list = {
-    'carnatic': {
-        'dunya-carnatic': {
-            'name': 'Dunya Carnatic',
-            'description': 'Dunya Carnatic',
-            'slug': 'dunya-carnatic',
-            'root_directory': '/incoming/Carnatic/',
-            'id': 7,
+    "carnatic": {
+        "dunya-carnatic": {
+            "name": "Dunya Carnatic",
+            "description": "Dunya Carnatic",
+            "slug": "dunya-carnatic",
+            "root_directory": "/incoming/Carnatic/",
+            "id": 7,
         },
-        'dunya-carnatic-cc': {
-            'name': 'Dunya Carnatic CC',
-            'description': 'Dunya CC collection in musicbrainz (basically the same as Saraga)',
-            'slug': 'dunya-carnatic-cc',
-            'root_directory': '/incoming/CarnaticCC',
-            'id': 18,
+        "dunya-carnatic-cc": {
+            "name": "Dunya Carnatic CC",
+            "description": "Dunya CC collection in musicbrainz (basically the same as Saraga)",
+            "slug": "dunya-carnatic-cc",
+            "root_directory": "/incoming/CarnaticCC",
+            "id": 18,
         },
     },
-    'hindustani': {
-        'dunya-hindustani-cc': {
-            'name': 'Dunya Hindustani CC',
-            'description': 'Creative commons licensed Hindustani music',
-            'slug': 'dunya-hindustani-cc',
-            'root_directory': '/incoming/HindustaniCC',
-            'id': 19,
+    "hindustani": {
+        "dunya-hindustani-cc": {
+            "name": "Dunya Hindustani CC",
+            "description": "Creative commons licensed Hindustani music",
+            "slug": "dunya-hindustani-cc",
+            "root_directory": "/incoming/HindustaniCC",
+            "id": 19,
         },
-        'dunya-hindustani': {
-            'name': 'Dunya Hindustani',
-            'description': 'Commercial Hindustani recordings',
-            'slug': 'dunya-hindustani',
-            'root_directory': '/incoming/Hindustani',
-            'id': 15,
+        "dunya-hindustani": {
+            "name": "Dunya Hindustani",
+            "description": "Commercial Hindustani recordings",
+            "slug": "dunya-hindustani",
+            "root_directory": "/incoming/Hindustani",
+            "id": 15,
         },
     },
 }
---
+++ tests/test_wrappers.py
@@ -0,0 +1,28 @@
+import pytest
+import mirdata
+from compiam.dunya import Corpora
+from compiam import list_models, load_dataset, load_corpora, \
+    list_datasets, list_corpora, load_model
+
+def test_load_dataset():
+    dataset = load_dataset("mridangam_stroke")
+    dataset_mirdata = mirdata.initialize("mridangam_stroke")
+    assert dataset == dataset_mirdata
+    with pytest.raises(ValueError):
+        load_dataset("hola")
+
+def test_load_corpora():
+    with pytest.raises(ValueError):
+        load_corpora("hola")
+    with pytest.raises(ValueError):
+        load_corpora("hola", cc="hola")
+    corpora = load_corpora("carnatic", token="hola")
+    assert type(corpora) == Corpora
+
+def test_lists():
+    assert type(list_models()) is list
+    assert type(list_datasets()) is list
+    assert type(list_corpora()) is list
+    assert "melody:melodia" in list_models()
+    assert "saraga_carnatic" in list_datasets()
+    assert "hindustani" in list_corpora()
