03729763e1e86594eef8dfeeee9f3018b8d102d7
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-10-03 18:01:02 +0200

    fix error in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import warnings
+
 
 def pytest_addoption(parser):
     parser.addoption("--tensorflow", action="store_true", default=False, help="optional tensorflow dependency")
@@ -20,16 +22,36 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--tensorflow"):
-        # --runslow given in cli: do not skip slow tests
+    if config.getoption("--tensorflow") and not config.getoption("--torch"):
+        skip_torch = pytest.mark.skip(reason="need --torch option to run")
+        for item in items:
+            if "torch" in item.keywords:
+                item.add_marker(skip_torch)
         return
-    if config.getoption("--torch"):
+    elif config.getoption("--torch") and not config.getoption("--tensorflow"):
         # --runslow given in cli: do not skip slow tests
+        skip_tensorflow = pytest.mark.skip(reason="need --tensorflow option to run")
+        for item in items:
+            if "tensorflow" in item.keywords:
+                item.add_marker(skip_tensorflow)
         return
-    skip_tensorflow = pytest.mark.skip(reason="need --tensorflow option to run")
-    skip_torch = pytest.mark.skip(reason="need --torch option to run")
-    for item in items:
-        if "tensorflow" in item.keywords:
-            item.add_marker(skip_tensorflow)
-        if "torch" in item.keywords:
-            item.add_marker(skip_torch)
+    elif config.getoption("--torch") and config.getoption("--tensorflow"):
+        warnings.warn("""
+            Cannot run tensorflow and torch tests at the same time.
+            Please use the --tensorflow and --torch flags separatedly.
+        """)
+        skip_tensorflow = pytest.mark.skip(reason="need --tensorflow option to run")
+        skip_torch = pytest.mark.skip(reason="need --torch option to run")
+        for item in items:
+            if "tensorflow" in item.keywords:
+                item.add_marker(skip_tensorflow)
+            if "torch" in item.keywords:
+                item.add_marker(skip_torch)
+    else:
+        skip_tensorflow = pytest.mark.skip(reason="need --tensorflow option to run")
+        skip_torch = pytest.mark.skip(reason="need --torch option to run")
+        for item in items:
+            if "tensorflow" in item.keywords:
+                item.add_marker(skip_tensorflow)
+            if "torch" in item.keywords:
+                item.add_marker(skip_torch)
