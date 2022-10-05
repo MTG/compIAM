43c9a8ea80abacdc3094db40ac9e4f4d0c439683
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-10-05 20:24:57 +0200

    add all changed files

--- compiam/__init__.py
+++ compiam/__init__.py
@@ -7,10 +7,11 @@ from compiam.data import models_dict, datasets_list, corpora_list
 from compiam.exceptions import ModelNotDefinedError
 
 def load_model(model_name, models_dict=models_dict):
-    """ Wrapper for loading models, extractors, and algorithms
-    :param model_name: name of the model, extractors, or algorithm to load
-    :param models_dict: dict object including the available models
-    :returns: specific Class of the selected model
+    """Wrapper for loading models, extractors, and algorithms.
+
+    :param model_name: name of the model, extractors, or algorithm to load.
+    :param models_dict: dict object including the available models.
+    :returns: specific Class of the selected model.
     """
     if not model_name in models_dict:
         raise ModelNotDefinedError(
@@ -24,25 +25,27 @@ def load_model(model_name, models_dict=models_dict):
     return module(**m_dict["kwargs"])
 
 def load_dataset(dataset_name, data_home=None, version="default"):
-    """ Wrapper/alias function to load a mirdata Dataset class
+    """Wrapper/alias function to load a mirdata Dataset class.
+
     :param dataset_name: the dataset's name, see mirdata.DATASETS for a 
-        complete list of possibilities
+        complete list of possibilities.
     :param data_home: path where the data lives. If None uses the default 
         home location.
     :param version: which version of the dataset to load. If None, the 
         default version is loaded.
-    :returns: a mirdata.core.Dataset object
+    :returns: a mirdata.core.Dataset object.
     """
     if dataset_name not in datasets_list:
         raise ValueError("Invalid dataset {}".format(dataset_name)) 
     return mirdata.initialize(dataset_name=dataset_name, data_home=data_home, version=version)
 
 def load_corpora(tradition, cc=True, token=None):
-    """ Wrapper function to load access to the Dunya corpora
-    :param tradition: carnatic or hindustani
-    :param cc: boolean indicating if the CC version of the corpora is loaded
-    :param token: Dunya personal access token
-    :returns: a compiam.Corpora object
+    """Wrapper function to load access to the Dunya corpora.
+
+    :param tradition: carnatic or hindustani.
+    :param cc: boolean indicating if the CC version of the corpora is loaded.
+    :param token: Dunya personal access token.
+    :returns: a compiam.Corpora object.
     """
     if tradition not in list(corpora_list.keys()):
         raise ValueError("Please enter a valid tradition in {}".format(list(corpora_list.keys())))
@@ -59,22 +62,25 @@ def load_corpora(tradition, cc=True, token=None):
     return Corpora(tradition=tradition, cc=cc, token=token)
 
 def list_models():
-    """ Just listing the available models
-    :returns: a list of available models
+    """Just listing the available models.
+
+    :returns: a list of available models.
     """
     return list(models_dict.keys())
 
 def list_datasets():
-    """ Just listing the available datasets
-    :returns: a list of available datasets
+    """Just listing the available datasets.
+
+    :returns: a list of available datasets.
     """
     return datasets_list
 
 def list_corpora():
-    """ Just listing the available corpora. For each corpora we do have
-        a restricted and a CC version. To indicate which of the two is
-        loaded with ``load_corpora`` function, use the input parameter
-        cc as True or False.
-    :returns: a list of available corpora
+    """Just listing the available corpora. For each corpora we do have
+            a restricted and a CC version. To indicate which of the two 
+            is loaded with ``load_corpora`` function, use the input 
+            parameter cc as True or False.
+
+    :returns: a list of available corpora.
     """
     return list(corpora_list.keys())
