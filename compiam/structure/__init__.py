97efa3bf15ecff8e75a32287020e33ae4587aa8e
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-11-04 15:30:48 +0100

    programatic list tools

--- compiam/structure/__init__.py
+++ compiam/structure/__init__.py
@@ -1,5 +1,10 @@
 from compiam.structure.dhrupad_bandish_segmentation import DhrupadBandishSegmentation
 
 # Show user the available tools
+import sys, inspect
 def list_tools():
-    return ["DhrupadBandishSegmentation"]
+    list_of_tools = []
+    for _, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj):
+            list_of_tools.append(obj.__name__)
+    return list_of_tools
--- compiam/timbre/__init__.py
+++ compiam/timbre/__init__.py
@@ -1,5 +1,10 @@
 from compiam.timbre.mridangam_stroke_classification import MridangamStrokeClassification
 
 # Show user the available tools
+import sys, inspect
 def list_tools():
-    return ["MridangamStrokeClassification"]
+    list_of_tools = []
+    for _, obj in inspect.getmembers(sys.modules[__name__]):
+        if inspect.isclass(obj):
+            list_of_tools.append(obj.__name__)
+    return list_of_tools
