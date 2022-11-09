eb8cf62e242b4150f69e058e894b3365d4081934
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-11-09 18:50:21 +0100

    try to apply template to main page

--- docs/conf.py
+++ docs/conf.py
@@ -34,13 +34,9 @@ extensions = [
 # The suffix of source filenames.
 source_suffix = ".rst"
 
-# The master toctree document.
-master_doc = "index"
-
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
