7e807d8e9ef886c10f64320cf7ed2de31805d154
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-11-10 20:16:17 +0100

    fix untracked change

--- compiam/rhythm/meter_tracking/akshara_pulse_tracker/__init__.py
+++ compiam/rhythm/meter_tracking/akshara_pulse_tracker/__init__.py
@@ -1,2 +1,2 @@
-from compiam.rhythm.meter.akshara_pulse_tracker.models import *
-from compiam.rhythm.meter.akshara_pulse_tracker import parameters as params
+from compiam.rhythm.meter_tracking.akshara_pulse_tracker.models import *
+from compiam.rhythm.meter_tracking.akshara_pulse_tracker import parameters as params
