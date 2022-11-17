ef1761e241dbb83073f96899871ebea9ddc51861
genisplaja <genis.plaja01@estudiant.upf.edu>
2022-11-17 19:28:05 +0100

    fix tests

--- tests/melody/test_deepsrgm.py
+++ tests/melody/test_deepsrgm.py
@@ -9,9 +9,13 @@ from compiam.data import WORKDIR
 
 def _load_model():
     deepsrgm = load_model("melody:deepsrgm")
-    with pytest.raises(ValueError):
+    with pytest.raises(FileNotFoundError):
         deepsrgm.predict(
             os.path.join(WORKDIR, "tests", "resources", "melody", "hola.wav")
+        )
+    with pytest.raises(ValueError):
+        deepsrgm.predict(
+            os.path.join(WORKDIR, "tests", "resources", "melody", "pitch_test.wav")
         )
     raga_mapping = deepsrgm.mapping
     assert raga_mapping == {
