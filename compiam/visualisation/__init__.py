28bcfab1608cf770ea2411504eae6f98c2e33e0d
Genís Plaja-Roglans <genis.plaja@upf.edu>
2023-02-15 14:24:46 +0100

    Update akshara pulse tracker (rhythm extractor) and fix tool listing functions (#84)
    * update list tools, move akshara class to __init__
    
    * update ipython version
    
    * downgrade ipython for python3.7
    
    * fix write and add read csv
    
    * fix bug in melodia and add write csv in pitch extractors
    
    * remove prova.csv
    
    * remove unneded function
    
    * change to save_pitch and update read_csv

--- compiam/visualisation/__init__.py
+++ compiam/visualisation/__init__.py
@@ -3,5 +3,4 @@ from compiam.visualisation.audio import *
 from compiam.visualisation.training import *
 from compiam.visualisation.peaks import *
 from compiam.visualisation.waveform_player import *
-
 from compiam.visualisation.pitch import *
