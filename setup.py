fbc480313884a341ade08950f876f46687e95358
thomasgnuttall <thomasgnuttall@gmail.com>
2023-11-06 10:55:42 +0100

    version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./requirements.txt") as f:
 
 setup(
     name="compiam",
-    version="0.1.0",
+    version="0.3.0",
     author_email=["genis.plaja@upf.edu", "thomas.nuttall@upf.edu"],
     zip_safe=False,
     include_package_data=True,
