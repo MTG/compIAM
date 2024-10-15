59589dd6663bbcdea0eb153f9abe60b1a64cf1cd
genisplaja <genis.plaja@upf.edu>
2024-10-15 18:47:55 +0200

    sampleRate-->sample_rate in deepsrgm

--- compiam/melody/raga_recognition/deepsrgm/__init__.py
+++ compiam/melody/raga_recognition/deepsrgm/__init__.py
@@ -219,7 +219,7 @@ class DEEPSRGM(object):
                 import essentia.standard as estd
 
                 melodia = compiam.melody.pitch_extraction.Melodia
-                melodia = melodia(sampleRate=self.sample_rate)
+                melodia = melodia(sample_rate=self.sample_rate)
                 tonic_extraction = (
                     compiam.melody.tonic_identification.TonicIndianMultiPitch
                 )
